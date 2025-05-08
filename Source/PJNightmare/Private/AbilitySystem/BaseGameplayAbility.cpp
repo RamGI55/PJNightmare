26df012c15287e5260c6aff87953ed118f5158a3
Hyunsoo Park <86937501+RamGI55@users.noreply.github.com>
2025-05-08 16:00:39 +0900

    Add the Gameplay Ability for the weapon

---
+++ Source/PJNightmare/Private/AbilitySystem/BaseGameplayAbility.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/BaseGameplayAbility.h"
+
+#include "AbilitySystemComponent.h"
+#include "Engine/SpecularProfile.h"
+
+
+void UBaseGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+
+	if (PlayerActivationPolicy == EBasePlayerActiviationPolicy::OnGiven)
+	{
+		// Activate the ability immediately when given
+		if (ActorInfo->IsLocallyControlled() && Spec.IsActive()) // in multiplayer enviornment 
+		{
+			ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
+		}
+	}
+}
+
+void UBaseGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+	
+	if (PlayerActivationPolicy == EBasePlayerActiviationPolicy::OnGiven)
+		if (ActorInfo->IsLocallyControlled())
+		{
+			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+		}
+}
---
+++ Source/PJNightmare/Public/AbilitySystem/BaseGameplayAbility.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "BaseGameplayAbility.generated.h"
+
+UENUM(BlueprintType)
+enum class EBasePlayerActiviationPolicy : uint8
+{
+	OnTriggered,
+	OnGiven
+};
+
+/**
+ * 
+ */
+UCLASS()
+class PJNIGHTMARE_API UBaseGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+	
+protected:
+	// GamePlay Ability System
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Player Ability")
+	EBasePlayerActiviationPolicy PlayerActivationPolicy = EBasePlayerActiviationPolicy::OnTriggered;
+	
+};
