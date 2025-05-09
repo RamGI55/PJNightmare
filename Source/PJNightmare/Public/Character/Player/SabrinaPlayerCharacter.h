6ed68778ddfc1f05b924405f863323dd3dce51cf
Hyunsoo Park <86937501+RamGI55@users.noreply.github.com>
2025-05-09 10:56:30 +0900

    update the childern of the characters
    Two main characters has been updated - with the placeholder assets

---
+++ Source/PJNightmare/Public/Character/Player/SabrinaPlayerCharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/Player/BasePlayerCharacter.h"
+#include "SabrinaPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PJNIGHTMARE_API ASabrinaPlayerCharacter : public ABasePlayerCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
